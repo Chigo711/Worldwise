228dc0844ce8a10ca1a9c6cc661cdda387dc7dd0
Ndubuaku Chigozie <victoriousman711@gmail.com>
2024-09-28 18:35:42 +0100

    Added the Context API for some state management

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,10 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
 import Message from "./Message"
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CityList({ cities, isLoading }) {
+export default function CityList() {
+  const {cities, isLoading} = useCities();
   if (isLoading) return <Spinner />;
   if(!cities.length) return <Message message="There is no city on the list, click on the map to add a city" />
   // console.log(cities);
